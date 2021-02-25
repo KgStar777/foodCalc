97eb579d9dd8404dabe0c1de660187aaeba0bf3c
Daniil Sychev <sych94@yandex.ru>
2021-02-26 01:25:54 +0300

    get data complited

--- fetch.js
+++ fetch.js
@@ -1,25 +1,55 @@
-const textInput = document.querySelector('input');
-const btn = document.querySelector('button');
-
 const app_id = 'e7899cb2';
 const app_key = '6354b3d70d643ce88f7d5763ef533b2b';
 
+const textInput = document.querySelector('input');
+const btn = document.querySelector('button');
+
+const nutrientsData = {
+    CHOCDF: 'углеводы',
+    ENERC_KCAL: 'энергетическая ценность',
+    FAT: 'жир',
+    FIBTG: 'клетчатка',
+    PROCNT: 'протеин'
+};
+
+let nutrientsTemplate = {
+    'углеводы': '',
+    'энергетическая ценность': '',
+    'жир': '',
+    'клетчатка': '',
+    'протеин': ''
+};
+
 function render(data) {
-    debugger;
-    let html = data.map(item => {
-    return `<div class="wrapper"><h2>${item.title}</h2><p>${item.body}</p></div>`
+    // console.log(data);
+    // console.log(data.hints);
+    console.log(data.hints[0].food.image);
+    let currentNutrients = data.food.nutrients.map(i => {
+        // return `<p><span></span></p>`
+        for(i in nutrientsTemplate) {
+            //деструктуризация объекта
+        }
     });
-    document.querySelector('.container').insertAdjacentHTML('beforebegin', html.join(' '));
+
+    let html = data.hints.map(i => {
+        return `<div class="wrapper"><div><img src='${i.food.image}'></div><h2>${i.food.label}</h2><p>${i.food.nutrients.FAT}</p></div>`;
+    })
+    document.querySelector('.container').insertAdjacentHTML('afterbegin', html.join(' '));
+    //----------------------------
+    // let hints = [];
+    // data.hints.map(i => {
+    //     console.log(i);
+    // })
 } 
 
 
 const requestURL = (food) => {
-    return `https://api.edamam.com/api/food-database/v2/parser?ingr=${food}&app_id=${app_id}&app_key=${app_key}`
+    return `https://api.edamam.com/api/food-database/v2/parser?ingr=${food}&app_id=${app_id}&app_key=${app_key}`;
 }
 
 btn.addEventListener('click', () => {
     
-    function sendRequest (method, url, body = null) {
+    function sendRequest (method, url) {
         return fetch(url)
         .then(response => {
             return response.json();
@@ -30,4 +60,4 @@ btn.addEventListener('click', () => {
 
     let food = textInput.value;
     sendRequest('GET', requestURL(food))
-})
+});
